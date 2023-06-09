e117b6ca355d23f38747696a93b1e521fc2a879a
CherukuRahul <96683756+CherukuRahul@users.noreply.github.com>
2023-06-09 18:05:43 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "If the HCF of 180 and 432 is expressed as (180m + 432n), where m and n are integers, then what is the difference between m and n?",
+    answer: "7",
+    options: [
+      "9",
+      "1",
+      "7",
+      "6"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The sum of two numbers is 2604 and their hcf is 124. Which is smaller between them if their difference is the least possible?",
+    answer: "1240",
+    options: [
+      "1801",
+      "1260",
+      "1234",
+      "1240"
+    ]
+  },
+    {
+    numb: 3,
+    question: "If HCF of two numbers is 17  and  LCM is 1309 , which   of the  given  options  could  be one of the two  numbers?",
+    answer: "119",
+    options: [
+      "119",
+      "42",
+      "50",
+      "34"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the lowest possible integer that is divisible by each of the integers 1 through 7, inclusive?",
+    answer: "420",
+    options: [
+      "450",
+      "76",
+      "326",
+      "420"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the least multiple of 13 which divided by 3, 4, 5, 8 and 10 leaves remainder 2 in  each case? ",
+    answer: "962",
+    options: [
+      "452",
+      "962",
+      "745",
+      "961"
+    ]
+  },
+];
